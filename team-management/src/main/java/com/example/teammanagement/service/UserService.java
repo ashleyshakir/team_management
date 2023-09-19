f17291ac9c8bbc339b2e2d9419aa822542c3e7ab
ashleyshakir <ashleymshakir@gmail.com>
2023-09-19 11:27:20 -0400

    loginUser POST method written for authenticating a user based on the login credentials

--- team-management/src/main/java/com/example/teammanagement/service/UserService.java
+++ team-management/src/main/java/com/example/teammanagement/service/UserService.java
@@ -2,13 +2,20 @@ package com.example.teammanagement.service;
 
 import com.example.teammanagement.exception.InformationExistException;
 import com.example.teammanagement.model.User;
+import com.example.teammanagement.model.request.LoginRequest;
 import com.example.teammanagement.repository.UserRepository;
 import com.example.teammanagement.security.JWTUtils;
+import com.example.teammanagement.security.MyUserDetails;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -47,6 +54,30 @@ public class UserService {
      */
     public User findUserByEmailAddress(String emailAddress){
         return userRepository.findUserByEmailAddress(emailAddress);
+    }
+
+    /**
+     * Authenticates a user based on the provided login credentials and
+     * generates a JWT upon successful authentication.
+     * @param loginRequest The LoginRequest object containing the user's email address and password.
+     * @return An Optional containing the generated JWT if authentication is successful; otherwise, an empty Optional.
+     */
+    public Optional<String> loginUser(LoginRequest loginRequest){
+        // Create an authentication token with the user's email address and password
+        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(loginRequest.getEmailAddress(),loginRequest.getPassword());
+        try{
+            // Authenticate the user using the AuthenticationManager
+            Authentication authentication = authenticationManager.authenticate(authenticationToken);
+            // Set the authenticated user's information in the security context
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+            // Obtain user details from the authenticated principal
+            MyUserDetails myUserDetails = (MyUserDetails) authentication.getPrincipal();
+            // Generate a JWT token for the authenticated user
+            return Optional.of(jwtUtils.generateJwtToken(myUserDetails));
+        } catch(Exception e){
+            // Return an empty Optional if authentication fails or an exception occurs
+            return Optional.empty();
+        }
     }
 
 
