8b6a3fdcc0ab1e01b895b7f6b41be7dad2c36d12
ashleyshakir <ashleymshakir@gmail.com>
2023-09-19 13:41:48 -0400

    getters and setters added for Coach class

--- team-management/src/main/java/com/example/teammanagement/model/Coach.java
+++ team-management/src/main/java/com/example/teammanagement/model/Coach.java
@@ -14,4 +14,45 @@ public class Coach {
     @Column
     private String lastName;
 
+    public Coach() {
+    }
+
+    public Coach(Long coachId, String firstName, String lastName) {
+        this.coachId = coachId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public Long getCoachId() {
+        return coachId;
+    }
+
+    public void setCoachId(Long coachId) {
+        this.coachId = coachId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "Coach{" +
+                "coachId=" + coachId +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
 }
