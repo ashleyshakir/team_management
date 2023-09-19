4b5dd8398f0dc04f463d80ec8915e3b27ec6a0bd
ashleyshakir <ashleymshakir@gmail.com>
2023-09-19 13:50:27 -0400

    Coach repository created

---
+++ team-management/src/main/java/com/example/teammanagement/repository/CoachRepository.java
@@ -0,0 +1,9 @@
+package com.example.teammanagement.repository;
+
+import com.example.teammanagement.model.Coach;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CoachRepository extends JpaRepository<Coach, Long> {
+}
