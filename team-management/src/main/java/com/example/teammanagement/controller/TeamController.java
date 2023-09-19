94c29a6de86178694a7a00009ebefc8ea54a8f7f
ashleyshakir <ashleymshakir@gmail.com>
2023-09-19 07:40:24 -0400

    GET all teams endpoint created in controller

--- team-management/src/main/java/com/example/teammanagement/controller/TeamController.java
+++ team-management/src/main/java/com/example/teammanagement/controller/TeamController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -24,5 +25,10 @@ public class TeamController {
     @GetMapping(path = "/teams/{teamId}")
     public Optional<Team> getTeam(@PathVariable(value = "teamId") Long teamId){
         return teamService.getTeam(teamId);
+    }
+
+    @GetMapping(path = "/teams/")
+    public List<Team> getTeams() {
+        return teamService.getTeams();
     }
 }
