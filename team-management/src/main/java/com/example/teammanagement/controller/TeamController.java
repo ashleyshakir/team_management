30d3f9424eb4bf30830410cb8767ce918db3c5cd
ashleyshakir <ashleymshakir@gmail.com>
2023-09-19 07:33:40 -0400

    team service autowired to team controller

--- team-management/src/main/java/com/example/teammanagement/controller/TeamController.java
+++ team-management/src/main/java/com/example/teammanagement/controller/TeamController.java
@@ -1,5 +1,8 @@
 package com.example.teammanagement.controller;
 
+import com.example.teammanagement.service.TeamService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,4 +10,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(path = "/api")
 public class TeamController {
 
+    private TeamService teamService;
+
+    @Autowired
+    public void setTeamService(TeamService teamService) {
+        this.teamService = teamService;
+    }
 }
