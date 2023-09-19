55a59bcd9b38addb6719be4283d576f75d380ef6
ashleyshakir <ashleymshakir@gmail.com>
2023-09-18 23:30:58 -0400

    UserRepository autowired to UserService class

--- team-management/src/main/java/com/example/teammanagement/repository/UserRepository.java
+++ team-management/src/main/java/com/example/teammanagement/repository/UserRepository.java
@@ -1,4 +1,10 @@
 package com.example.teammanagement.repository;
 
-public class UserRepository {
+import com.example.teammanagement.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
 }
--- team-management/src/main/java/com/example/teammanagement/service/UserService.java
+++ team-management/src/main/java/com/example/teammanagement/service/UserService.java
@@ -1,4 +1,15 @@
 package com.example.teammanagement.service;
 
+import com.example.teammanagement.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserService {
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
 }
