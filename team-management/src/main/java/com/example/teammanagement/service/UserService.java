affc790e7b4926f93a40a399f557497a06fbc5f6
ashleyshakir <ashleymshakir@gmail.com>
2023-09-18 23:41:10 -0400

    createUser business logic written

--- team-management/src/main/java/com/example/teammanagement/service/UserService.java
+++ team-management/src/main/java/com/example/teammanagement/service/UserService.java
@@ -1,20 +1,34 @@
 package com.example.teammanagement.service;
 
+import com.example.teammanagement.exception.InformationExistException;
 import com.example.teammanagement.model.User;
 import com.example.teammanagement.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Autowired
-    public UserService(UserRepository userRepository){
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder){
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
+    /**
+     * Registers a new user.
+     * @param userObject The user object to be registered.
+     * @return Returns the registered user object.
+     */
     public User createUser(User userObject){
-        return userObject;
+        if(!userRepository.existsByEmailAddress(userObject.getEmailAddress())){
+            userObject.setPassword(passwordEncoder.encode(userObject.getPassword()));
+            return userRepository.save(userObject);
+        } else{
+            throw new InformationExistException("User with email address " + userObject.getEmailAddress() + " already exists.");
+        }
     }
 }
