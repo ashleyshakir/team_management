4c4c0415c1fbeb6beaab85c3508fd02313bebd28
ashleyshakir <ashleymshakir@gmail.com>
2023-09-19 07:46:26 -0400

    POST endpoint for creating a team created in controller

--- team-management/src/main/java/com/example/teammanagement/controller/TeamController.java
+++ team-management/src/main/java/com/example/teammanagement/controller/TeamController.java
@@ -3,10 +3,7 @@ package com.example.teammanagement.controller;
 import com.example.teammanagement.model.Team;
 import com.example.teammanagement.service.TeamService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -20,6 +17,11 @@ public class TeamController {
     @Autowired
     public void setTeamService(TeamService teamService) {
         this.teamService = teamService;
+    }
+
+    @PostMapping(path = "/teams/")
+    public Team createTeam(@RequestBody Team teamObject){
+        return teamService.createTeam(teamObject);
     }
 
     @GetMapping(path = "/teams/{teamId}")
