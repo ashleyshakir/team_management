a7f7e3137e1d94ef2876c2e914d43175455fb09a
ashleyshakir <ashleymshakir@gmail.com>
2023-09-19 07:17:39 -0400

    team repository autowired to team service

--- team-management/src/main/java/com/example/teammanagement/service/TeamService.java
+++ team-management/src/main/java/com/example/teammanagement/service/TeamService.java
@@ -1,7 +1,17 @@
 package com.example.teammanagement.service;
 
+import com.example.teammanagement.model.Team;
+import com.example.teammanagement.repository.TeamRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TeamService {
+
+    private TeamRepository teamRepository;
+
+    @Autowired
+    public void setTeamRepository(TeamRepository teamRepository){
+        this.teamRepository = teamRepository;
+    }
 }
