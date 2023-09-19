6eeadbdf49a5ce7b78e556e57e0a6029a74348c7
ashleyshakir <ashleymshakir@gmail.com>
2023-09-19 05:21:59 -0400

    UserService autowired to USerController class

--- team-management/src/main/java/com/example/teammanagement/controller/UserController.java
+++ team-management/src/main/java/com/example/teammanagement/controller/UserController.java
@@ -1,4 +1,17 @@
 package com.example.teammanagement.controller;
 
+import com.example.teammanagement.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(path = "/auth/users")
 public class UserController {
+    private UserService userService;
+
+    @Autowired
+    public void setUserService(UserService userService){
+        this.userService = userService;
+    }
 }
