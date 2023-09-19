8e8ad8a830f84890e648436573ca495b1b38263b
ashleyshakir <ashleymshakir@gmail.com>
2023-09-19 07:32:16 -0400

    GET method created for getting a single team

--- team-management/src/main/java/com/example/teammanagement/service/TeamService.java
+++ team-management/src/main/java/com/example/teammanagement/service/TeamService.java
@@ -1,9 +1,12 @@
 package com.example.teammanagement.service;
 
+import com.example.teammanagement.exception.InformationNotFoundException;
 import com.example.teammanagement.model.Team;
 import com.example.teammanagement.repository.TeamRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class TeamService {
@@ -14,4 +17,18 @@ public class TeamService {
     public void setTeamRepository(TeamRepository teamRepository){
         this.teamRepository = teamRepository;
     }
+
+    /**
+     * Retrieve a Team object from the database by its id.
+     * @param teamId The unique id of the team to be retrieved.
+     * @return The team object.
+     */
+    public Optional<Team> getTeam(Long teamId){
+        Optional<Team> team = teamRepository.findById(teamId);
+        if(team.isEmpty()){
+            throw new InformationNotFoundException("Team with id " + teamId + " not found.");
+        }
+        return team;
+    }
+
 }
