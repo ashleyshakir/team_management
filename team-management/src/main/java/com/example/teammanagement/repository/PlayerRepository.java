c592c5b309e679d2967dce87cbad7b3117f5b187
ashleyshakir <ashleymshakir@gmail.com>
2023-09-19 16:16:14 -0400

    findBy methods added to Player Repository

--- team-management/src/main/java/com/example/teammanagement/repository/PlayerRepository.java
+++ team-management/src/main/java/com/example/teammanagement/repository/PlayerRepository.java
@@ -1,10 +1,25 @@
 package com.example.teammanagement.repository;
 
+import com.example.teammanagement.model.Coach;
 import com.example.teammanagement.model.Player;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PlayerRepository extends JpaRepository<Player, Long> {
+    /**
+     * Find a player by their name.
+     * @param name The player's name.
+     * @return Return the player object with the specified name.
+     */
+    Player findByName(String name);
+    /**
+     * Retrieve a list of players belonging to a team with a specific ID.
+     * @param teamId The ID of the team to retrieve the list of players from.
+     * @return A list of players belonging to the specific team.
+     */
+    List<Player> findByTeam_TeamId(Long teamId);
 
 }
