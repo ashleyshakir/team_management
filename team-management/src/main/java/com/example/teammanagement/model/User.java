174448f31e0b6975afc70a0aaf5fdebf2f5f865b
ashleyshakir <ashleymshakir@gmail.com>
2023-09-18 23:22:48 -0400

    getters and setters created for User model

--- team-management/src/main/java/com/example/teammanagement/model/User.java
+++ team-management/src/main/java/com/example/teammanagement/model/User.java
@@ -16,4 +16,45 @@ public class User {
     @Column
     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private String password;
+
+    public User() {
+    }
+
+    public User(Long userId, String emailAddress, String password) {
+        this.userId = userId;
+        this.emailAddress = emailAddress;
+        this.password = password;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "userId=" + userId +
+                ", emailAddress='" + emailAddress + '\'' +
+                '}';
+    }
 }
